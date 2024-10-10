15b315e3a3de6a4575aed6a19fea993e55005e42
Youko509 <saintcharleserico@gmail.com>
2024-10-10 10:42:08 -0400

    auto create user in thinkific

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, reverse, include
 from .views import *
 
 urlpatterns = [
+    path('signup/', MySignupView.as_view(), name='account_signup'),
     path('', include('allauth.urls')),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,19 @@
 from django.shortcuts import render
+from allauth.account.views import SignupView
+from django.conf import settings
+from thinkific import Thinkific
+thinkific = Thinkific(settings.THINKIFIC['AUTH_TOKEN'],"")
+class MySignupView(SignupView):
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+       
+        thinkific.users.create_user({
+            'email':self.user.email,
+            'first_name':self.user.first_name,
+            'last_name':self.user.last_name,
+            'full_name':f'{self.user.first_name} {self.user.last_name}',
+            'password':self.user.password
+        })
+
+        return response
