d15c00d2021afa931d41311ec53d6b4823b05386
Youko509 <saintcharleserico@gmail.com>
2024-10-09 21:16:33 -0400

    add empty string

--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from thinkific import Thinkific
 from django.conf import settings
-thinkific = Thinkific(settings.THINKIFIC['AUTH_TOKEN'],"<subdomain>")
+thinkific = Thinkific(settings.THINKIFIC['AUTH_TOKEN'],"")
 
 # Create your views here.
 def courses(request):
