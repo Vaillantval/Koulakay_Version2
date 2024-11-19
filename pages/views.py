49a0725a50a44fbd702d6f093f795d8782b68692
Youko509 <saintcharleserico@gmail.com>
2024-11-19 13:54:21 -0500

    update course annd course details

--- pages/views.py
+++ pages/views.py
@@ -3,15 +3,19 @@ from thinkific import Thinkific
 from django.conf import settings
 thinkific = Thinkific(settings.THINKIFIC['AUTH_TOKEN'],settings.THINKIFIC['SITE_ID'])
 from django.core.paginator import Paginator
-
+from courses.models import Enrollment
 # Create your views here.
 def home(request):
     courses = thinkific.courses.list()
     courses_items = courses['items']
     paginator = Paginator(courses_items, 5)  # Show 25 contacts per page.
-
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
+    if request.user.is_authenticated:
+        for c in courses_items:
+            e = Enrollment.objects.filter(user=request.user.pk,course_id=c['id'])
+            if e :
+                c['enroll']=True
     return render(request, 'pages/home.html', {'courses':page_obj})
     
 def contact(request):
