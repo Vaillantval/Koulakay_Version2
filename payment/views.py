e00c80bc1ac2617aea1aa6143e322bb6c877c732
Youko509 <saintcharleserico@gmail.com>
2024-11-21 07:46:13 -0500

    update code

--- payment/views.py
+++ payment/views.py
@@ -1,22 +1,16 @@
-from django.shortcuts import redirect
 from django.http import HttpResponseNotFound ,HttpResponseNotAllowed
 from django.conf import settings
-from django.contrib.sites.shortcuts import get_current_site
-
 from courses.models import Enrollment
 from .models import Transaction, Payment
-
 from django.utils import timezone
-from django.contrib.sites.shortcuts import get_current_site
 from datetime import timedelta
 from django.contrib.auth import get_user_model
 from thinkific import Thinkific
-thinkific = Thinkific(settings.THINKIFIC['AUTH_TOKEN'],settings.THINKIFIC['SITE_ID'])
-from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 import json
 from django.http import JsonResponse
 
+thinkific = Thinkific(settings.THINKIFIC['AUTH_TOKEN'],settings.THINKIFIC['SITE_ID'])
 
 @csrf_exempt
 def confirm(request):
@@ -26,37 +20,39 @@ def confirm(request):
         try:
             # Parse the incoming JSON payload
             payload = json.loads(request.body)
-            transaction_number=payload.get('paylink')['meta_data']['transaction_number']
+            transaction_number=payload.get('paylink').get('meta_data').get('transaction_number')
+            print(transaction_number)
             if transaction_number is not None:
                 try:
                     transaction = Transaction.objects.get(transaction_number=transaction_number)
                 except:
-                    return HttpResponseNotFound()
+                    return JsonResponse({'success': False, 'error': 'Transaction not found'}, status=404)
 
                 try:
 
-                    user     = get_user_model().objects.get(id=transaction.meta_data["user"]["id"])
-                    course_id = transaction.meta_data['course']["course_id"]
-                    thinkific_user_id = transaction.meta_data['user']["thinkific_user_id"]
+                    user     = get_user_model().objects.get(id=transaction.meta_data.get("user").get("id"))
+                    course_id = transaction.meta_data.get('course').get("course_id")
+                    thinkific_user_id = transaction.meta_data.get('user').get("thinkific_user_id")
                     activated_at = timezone.now()
                     expiry_date = (timezone.now() + timedelta(days=30))
+                   
+                    if course_id is not None and thinkific_user_id is not None:
+                        thinkific.enrollments.create_enrollment({
+                        'user_id':thinkific_user_id,
+                        'course_id':course_id,
+                        'activated_at':activated_at.isoformat(),
+                        'expiry_date':expiry_date.isoformat()
+                        })
+                    
+                        enrollment,created=Enrollment.objects.update_or_create(user=user, thinkific_user_id=thinkific_user_id,course_id=course_id,activated_at=activated_at,expiry_date=expiry_date)
 
-                    thinkific.enrollments.create_enrollment({
-                    'user_id':thinkific_user_id,
-                    'course_id':course_id,
-                    'activated_at':activated_at.isoformat(),
-                    'expiry_date':expiry_date.isoformat()
-                    })
-                
-                    enrollment,created=Enrollment.objects.update_or_create(user=user, thinkific_user_id=thinkific_user_id,course_id=course_id,activated_at=activated_at,expiry_date=expiry_date)
+                        Payment.objects.create(user=user,enrollment=enrollment,transaction=transaction)
 
-                    Payment.objects.create(user=user,enrollment=enrollment,transaction=transaction)
-
-                    transaction.status = Transaction.Status.COMPLETE
-                    transaction.save()
+                        transaction.status = Transaction.Status.COMPLETE
+                        transaction.save()
 
                 except get_user_model().DoesNotExist:
-                    return HttpResponseNotFound()
+                    return JsonResponse({'success': False, 'error': 'NOt found user'}, status=400)
                 
                
 
